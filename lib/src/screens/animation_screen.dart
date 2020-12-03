279f13e86c70cd5612f2a1cf5bf6d276a3b55a27
Your Name <hossamomar9876@gmail.com>
2020-12-03 16:27:50 +0200

    animated builder

--- lib/src/screens/animation_screen.dart
+++ lib/src/screens/animation_screen.dart
@@ -20,11 +20,11 @@ class AnimationScreenState extends State<AnimationScreen>
     // Init tomController
     tomContoller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2),
+      duration: Duration(milliseconds: 200),
     );
 
     // init tomAnimation
-    tomAnimation = Tween(begin: 0.0, end: 100.0).animate(
+    tomAnimation = Tween(begin: 70.0, end: 170.0).animate(
       CurvedAnimation(
         curve: Curves.easeIn,
         parent: tomContoller,
@@ -34,10 +34,48 @@ class AnimationScreenState extends State<AnimationScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Cat(),
+    return GestureDetector(
+      child: Center(
+        child: Stack(
+          overflow: Overflow.visible,
+          children: [
+            buildTomAnimation(),
+            buildBox(),
+          ],
+        ),
       ),
+      onTap: onTap,
     );
+  }
+
+  // create helper method for builder
+  Widget buildTomAnimation() {
+    return AnimatedBuilder(
+      animation: tomAnimation,
+      builder: (context, child) {
+        return Positioned(
+          bottom: tomAnimation.value,
+          child: child,
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  // create helper method for box
+  Widget buildBox() {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      color: Color(0xff2980b9),
+    );
+  }
+
+  void onTap() {
+    if (tomContoller.status == AnimationStatus.completed) {
+      tomContoller.reverse();
+    } else if (tomContoller.status == AnimationStatus.dismissed) {
+      tomContoller.forward();
+    }
   }
 }
